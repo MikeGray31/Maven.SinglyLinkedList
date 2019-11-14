d3ff3b01e56da93914057740fa992f8d096dc61f
MikeGray31 <michaelkgray31@gmail.com>
2019-11-14 18:43:29 -0500

    Update

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,145 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<E extends Comparable<E>>  implements Cloneable{
+
+    // fields ------------------------------
+
+    private int currentSize;
+    private Node<E> lastNode;
+    private Node<E> firstNode;
+
+
+    // constructor -------------------------
+
+    SinglyLinkedList(){
+        currentSize = 0;
+    }
+
+    // Node class --------------------------
+
+    private static class Node<E> implements Cloneable{
+        E data;
+        SinglyLinkedList.Node<E> next;
+        SinglyLinkedList.Node<E> prev;
+
+        Node(SinglyLinkedList.Node<E> prev, E data, SinglyLinkedList.Node<E> next) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    // methods -----------------------------
+
+    void add(E newData){
+
+        if(currentSize == 0){
+            Node<E> node = new Node<E>(null,newData,null);
+            this.firstNode = node;
+            this.lastNode = node;
+            this.currentSize = 1;
+        }
+        else {
+            Node<E> node = new Node<E>(this.lastNode, newData, null);
+            node.prev.next = node;
+            this.lastNode = node;
+            this.currentSize++;
+        }
+    }
+
+    void remove(int index){
+        SinglyLinkedList.Node currentNode = this.firstNode;
+        for(int i = 0; i < index; i++){
+            currentNode = currentNode.next;
+        }
+        currentNode.next.prev = currentNode.prev;
+        currentNode.prev.next = currentNode.next;
+    }
+
+    boolean contains(E dataToFind){
+        return this.find(dataToFind) > -1;
+    }
+
+    int find(E dataToFind){
+
+        int index = 0;
+        SinglyLinkedList.Node currentNode;
+
+        if (dataToFind == null) {
+            for(currentNode = this.firstNode; currentNode != null; currentNode = currentNode.next) {
+                if (currentNode.data == null) {
+                    return index;
+                }
+                ++index;
+            }
+        }
+
+        else {
+            for(currentNode = this.firstNode; currentNode != null; currentNode = currentNode.next) {
+                if (dataToFind.equals(currentNode.data)) {
+                    return index;
+                }
+                ++index;
+            }
+        }
+
+        return -1;
+    }
+
+    int size(){
+        return this.currentSize;
+    }
+
+    E get(int index){
+        SinglyLinkedList.Node getThis = this.firstNode;
+        for(int i = 0; i < index; i++){
+            getThis = getThis.next;
+        }
+        return (E) getThis.data;
+    }
+
+    SinglyLinkedList<E> copy(){
+
+        SinglyLinkedList<E> newClone = new SinglyLinkedList<>();
+        Node<E> currentNode = this.firstNode;
+
+        for(int i = 0; i < this.size(); i++){
+            newClone.add(currentNode.data);
+            currentNode = currentNode.next;
+        }
+        return newClone;
+    }
+
+    private void sort(){
+        Node<E> currentNode = this.firstNode;
+        if (){
+
+        }
+    }
+
+    private <E extends Comparable<E>> void swap( int i, int j) {
+        if (i != j) {
+
+
+        }
+    }
+
+    public <E extends Comparable<E>> void selectionSort() {
+        for (int i = 0; i < this.size() - 1; i++) {
+            // find index of smallest element
+            int smallest = i;
+            for (int j = i + 1; j < this.size(); j++) {
+                if (this.get(j).compareTo(this.get(smallest)) <= 0) {
+                    smallest = j;
+                }
+            }
+
+            swap(i, smallest);  // swap smallest to front
+        }
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,76 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    SinglyLinkedList<Integer> sLL;
+
+    @Before
+    public void doThisFirst(){
+        sLL = new SinglyLinkedList<>();
+        sLL.add(1);
+        sLL.add(2);
+        sLL.add(3);
+    }
+
+    @Test
+    public void instantiateTest1(){
+        SinglyLinkedList<Integer> newSLL = new SinglyLinkedList<Integer>();
+        Assert.assertTrue(newSLL instanceof SinglyLinkedList);
+        Assert.assertEquals(0, newSLL.size());
+    }
+
+    @Test
+    public void ContainsTest(){
+        Assert.assertEquals(3, sLL.size());
+        Assert.assertTrue(sLL.contains(1));
+        Assert.assertTrue(sLL.contains(2));
+        Assert.assertTrue(sLL.contains(3));
+    }
+
+    @Test
+    public void addTest(){
+
+        sLL.add(4);
+        Assert.assertEquals(4, sLL.size());
+        Assert.assertTrue(sLL.contains(4));
+    }
+
+
+    @Test
+    public void findTest(){
+        Assert.assertEquals(0, sLL.find(1));
+        Assert.assertEquals(1, sLL.find(2));
+        Assert.assertEquals(2, sLL.find(3));
+    }
+
+    @Test
+    public void getTest(){
+        Assert.assertEquals(1, sLL.get(0),0.0);
+        Assert.assertEquals(2, sLL.get(1),0.0);
+        Assert.assertEquals(3, sLL.get(2),0.0);
+    }
+
+    @Test
+    public void cloneTest(){
+        SinglyLinkedList<Integer> sLL2 = sLL.copy();
+        Assert.assertEquals(sLL.get(0), sLL2.get(0),0.0);
+        Assert.assertEquals(sLL.get(1), sLL2.get(1),0.0);
+        Assert.assertEquals(sLL.get(2), sLL2.get(2),0.0);
+
+        sLL2.add(4);
+        Assert.assertFalse(sLL.contains(4));
+        Assert.assertTrue(sLL2.contains(4));
+
+        sLL2.remove(1);
+        Assert.assertTrue(sLL.contains(2));
+        Assert.assertFalse(sLL2.contains(2));
+
+    }
 }
